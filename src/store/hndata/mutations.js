887ca4174f50c7c7c64cfea67b16688399620ee2
piutranq <piutranq@hotmail.com>
2021-02-21 05:35:56 +0900

    feat: Store data in Vuex store
    - `@lib/store/hndata/`
      - `mutations.js`: put the data to state directly, without validation
      - `state.js`: store the data from mutations

--- src/store/hndata/mutations.js
+++ src/store/hndata/mutations.js
@@ -1,3 +1,9 @@
-const mutations = {}
+const mutations = {
+  replaceItem (state, item) { state.item = item },
+  replaceUser (state, user) { state.user = user },
+  replaceMax (state, max) { state.max = max },
+  replaceUpdates (state, updates) { state.updates = updates },
+  replaceStories (state, obj) { state.stories[obj.key] = obj.list }
+}
 
 export default mutations
--- src/store/hndata/state.js
+++ src/store/hndata/state.js
@@ -1,3 +1,16 @@
-const state = {}
+const state = {
+  item: {},
+  user: {},
+  max: 0,
+  updates: {},
+  stories: {
+    new: [],
+    top: [],
+    best: [],
+    ask: [],
+    show: [],
+    job: []
+  }
+}
 
 export default state
