77b3824cfb6f79fe80733bb2195cd8c0ffa53eb0
piutranq <piutranq@hotmail.com>
2021-02-27 10:06:45 +0900

    feat: display newstories preview in '/news' route
    - just displaying title and url (link)
      - TODO: make the preview component for each story
    
    - make getters for 'hndata' Vuex module

--- src/store/hndata/getters.js
+++ src/store/hndata/getters.js
@@ -1,3 +1,12 @@
-const getters = {}
+const getters = {
+  item: state => state.item,
+  user: state => state.user,
+  max: state => state.max,
+  updates: state => state.updates,
+  updatedItems: state => state.updates.items,
+  updatedProfiles: state => state.updates.profiles,
+  stories: state => state.stories,
+  previews: state => state.previews
+}
 
 export default getters
--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,17 +1,33 @@
 <template>
   <div>
     <h1>News</h1>
+    <ul>
+      <li v-for="story in this['hndata/previews']"
+        v-bind:key="story.id">
+        <!-- TODO: Make the preview component for each story -->
+        <a v-bind:href="story.url" v-text="story.title"/>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+import { mapActions, mapGetters } from 'vuex'
 
 export default {
   created: async function () {
-    const type = 'newstories'
+    const type = this.type
     await this['hndata/fetchPreviews']({ type })
-    // console.log(this.$store.state.hndata.previews)
+  },
+  data () {
+    return {
+      type: 'newstories'
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'hndata/previews'
+    ])
   },
   methods: {
     ...mapActions([
